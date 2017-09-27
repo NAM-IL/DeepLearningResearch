9c992126a98cde697f3aa4269400bf63b6b5e7cc
NAM-IL <newlaser@hanmail.net>
2017-09-28 01:17:31 +0900

    test

---
+++ NumPy_Example/ex1_numpy.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from IPython.display import Image
+import matplotlib   
+import matplotlib.pyplot as plt
+
+sess = tf.InteractiveSession()
+print("Tensorflow version : ", tf.__version__)
+print("matplotlib version : ", matplotlib.__version__)
+
+ 
+ 
+ 
